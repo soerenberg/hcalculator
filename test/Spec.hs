862b9295deb728c7c84e865f3803393d860f946d
Soeren Berg <soeren.berg@protonmail.com>
2022-09-09 22:21:15 +0200

    Setup testing framework

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Lib.Internal
+import Test.Tasty
+import Test.Tasty.HUnit
+
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests"
+  [ testExtractRight
+  ]
+
+testExtractRight = testCase "extractRight" $ (extractRight $ Right "a") @?= "a"
