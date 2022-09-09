9a3d112ce183d02c26476ad27a6c03fce89cd6c7
Soeren Berg <soeren.berg@protonmail.com>
2022-09-10 00:48:29 +0200

    Tests for eval function, restructure TestTree.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,28 @@
 import Lib.Internal
 import Test.Tasty
 import Test.Tasty.HUnit
+import Text.ParserCombinators.Parsec
 
 main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Tests"
-  [ testExtractRight
+  [ testCase "extractRight" $ (extractRight $ Right "a") @?= "a"
+  , evalTests
+  , parseTests
   ]
 
-testExtractRight = testCase "extractRight" $ (extractRight $ Right "a") @?= "a"
+evalTests :: TestTree
+evalTests = testGroup "EvalTests"
+  [ testCase "eval 3+4" $ (eval (Add (Atom 3) (Atom 4))) @?= (Right 7)
+  , testCase "eval 7-3" $ (eval (Sub (Atom 7) (Atom 3))) @?= (Right 4)
+  , testCase "eval 3*2" $ (eval (Mul (Atom 3) (Atom 2))) @?= (Right 6)
+  , testCase "eval 10/4" $ (eval (Div (Atom 10) (Atom 4))) @?= (Right 2.5)
+  ]
+
+parseTests :: TestTree
+parseTests = testGroup "ParseTests"
+  [ testCase "parse '2'" $ parse parseNumber "hcalc" "2" @?= (Right . Atom $ 2)
+  , testCase "parse '4 '" $ 
+      parse parseNumber "hcalc" "4 " @?= (Right . Atom $ 4)
+  ]
